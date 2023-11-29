ef61872b53d3c557626ac6128067e59efd7f97a3
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 10:05:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,12 @@ if data_file is not None:
   fig, ax = plt.subplots()
   sns.heatmap(df.corr(method = 'pearson'),ax =ax,vmax =1,square = True,annot=True,cmap='Reds')
   st.write(fig)
+
+  output = st.rario('Choose a dependent variable', df.columns)
+  st.header('Show relationship between cariables')
+  for col in list(df.columns):
+    fig,ax=plt.subplots()
+    ax.scatter(x=df[col],y=df[output])
+    plt.xlabel(col)
+    plt.ylabel(output)
+    st.pyplot(fig)

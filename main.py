3f2d5dfff492289e34bfb6afdd9a1cc49dc597e7
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 09:31:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if data_file is not None:
   #Visualize each attribute
   st.header('Visualize each attribute')
   for col in list(df.columns):
-    fix, ax = plt.subplots()
+    fig, ax = plt.subplots()
     ax.hist(df[col],bins = 20)
     plt.xlabel(col)
     plt.ylabel('Quality')

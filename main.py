b89330493275388de7d72e7d1a856e30d7a18e1d
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 08:56:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 #add header
 st.title('Data Visualization')
 #upload data
-data_file = st.file_uploader('choose a csv file',type=(['.csv]))
+data_file = st.file_uploader('choose a csv file',type=(['.csv']))
 if data_file is not None:
   df = pd.read_csv(data_file)
   st.dataframe(df)

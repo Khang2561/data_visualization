768323e4896f6fe72ca7eb2903fde3837b243bd2
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 09:11:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #import lib
 import streamlit as st
 import pandas as pd
+import io
 #add header
 st.title('Data Visualization')
 #upload data
@@ -10,6 +11,11 @@ if data_file is not None:
   df = pd.read_csv(data_file)
   st.header('Show data')
   st.dataframe(df)
-
+  #add describe
   st.header('Descriptive statistics')
   st.table(df.describe())
+  #add info
+  st.header('Show data info')
+  buffer = io.StringIO()
+  df.info(buf=buffer)
+  df.text(buffer.getvalue())

ba71f981acbdf2ed185b0b5f7aed11d68a6343af
Nguyễn Phúc Khang <98502471+Khang2561@users.noreply.github.com>
2023-11-29 09:57:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if data_file is not None:
     plt.ylabel('Quality')
     st.pyplot(fig)
 
-    st.header('Show correlation between varibles')
-    fig, ax = plt.subplots()
-    sns.heatmap(df.corr(method = 'pearson'),ax =ax,vmax =1,square = True,annot=True,cmap='Reds')
-    st.write(fig)
+  st.header('Show correlation between varibles')
+  fig, ax = plt.subplots()
+  sns.heatmap(df.corr(method = 'pearson'),ax =ax,vmax =1,square = True,annot=True,cmap='Reds')
+  st.write(fig)
